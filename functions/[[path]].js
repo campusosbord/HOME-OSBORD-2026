9a262129cfdc06c33d2b082f25dc3d2477b96ec1
campusosbord <osbordnet@gmail.com>
2026-04-24 21:20:29 -0400

    fix: remove functions, use _worker.js only

--- functions/[[path]].js
+++
@@ -1,9 +0,0 @@
-// Cloudflare Pages Function - SSR Handler
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  
-  // Import the Astro SSR handler dynamically
-  const { default: handler } = await import('../dist/server/entry.mjs');
-  
-  return handler(context.request, context.env);
-}
