fabf1114c51b354ae702171c2c3e13a6b4ced14b
campusosbord <osbordnet@gmail.com>
2026-04-24 20:47:29 -0400

    fix: configure for Cloudflare Workers deployment

--- public/_worker.js
+++
@@ -1,5 +0,0 @@
-// Cloudflare Pages Worker Entry Point
-// SSR Handler for Astro - placed in public/ to be copied to dist/
-
-import * as server from './server/entry.mjs';
-export default server.default;
